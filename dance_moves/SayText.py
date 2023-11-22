ce6fa933e0e9537a65d0198d5d15400350aafc5a
jana <jnikolovskaa@gmail.com>
2023-11-23 00:10:03 +0100

    [RobotPositions][dance_moves] rename RobotPositions to dance_moves, remove text lines before every move, remove/add moves that are decided on

---
+++ dance_moves/SayText.py
@@ -0,0 +1,26 @@
+import time
+from naoqi import ALProxy
+import sys
+
+
+def main(robotIP, port,text):
+    try:
+        ttsProxy = ALProxy("ALTextToSpeech", robotIP, port)
+        ttsProxy.say(text)
+
+    except Exception, e:
+        print "Could not create proxy to ALTextToSpeech"
+        print "Error was: ", e
+
+    time.sleep(2)
+
+if __name__ == "__main__":
+
+    robotIP = sys.argv[1]
+    port = int(sys.argv[2])
+    if len(sys.argv) <= 3:
+        text = 'Woo'
+    else:
+        text = sys.argv[3]
+
+    main(robotIP, port,text)
