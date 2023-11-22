ae799441b5496d5d51f92b8006111694bb4dba83
jana <jnikolovskaa@gmail.com>
2023-11-23 00:11:43 +0100

    [tequila.mp3] add music file [meta_data/metadata.csv] add test metadata file, [create_metafile][find_conflicts] initial comit for extracting the metafile

---
+++ create_metafile.py
@@ -0,0 +1,28 @@
+import time
+import os
+import pandas as pd
+import numpy as np
+from utils import *
+
+config = get_config()
+
+
+meta_data = []
+directory = "dance_moves//"
+file_list = os.listdir('dance_moves')
+
+def calculate_duration(config,path):
+    start_time = time.time()
+    statement = "python " + path + " " + config['ip'] + " " + str(config['port'])
+    os.system(statement)
+    return round(float(time.time() - start_time),4)
+
+for file_name in file_list:
+    entry = {}
+    entry['MoveName'] = file_name
+    entry['Duration'] = calculate_duration(config,directory+file_name)
+    meta_data.append(entry)
+
+meta_df = pd.DataFrame().from_records(meta_data)
+meta_df.to_csv("meta_data//MetaData-Duration.csv")
+print('File created')
---
+++ find_conflicts.py
@@ -0,0 +1,45 @@
+import os
+import time
+import pandas as pd
+import numpy as np
+from nao import Move
+
+def eval_conditions(st):
+    if pd.isna(st):
+        return dict()
+    st = st.replace(";",",")
+    return eval(st)
+
+directory = "dance_moves//"
+
+#read metadata file and initialize all states
+metadata = pd.read_csv('meta_data//MetaData-Duration.csv')
+file_list = os.listdir('dance_moves')
+ip = "127.0.0.1"
+port = 49436
+
+moves = dict()
+# for index, row in metadata.iterrows():
+#     #name, duration, preconditions, postconditions,popularity
+#     moves[row['MoveName'][:-3]]=(Move(row['MoveName']+'.py',row['MoveName'],float(row['Duration'])))
+
+move1 = 'AirGuitar'
+move2 = 'SitRelax'
+move3 = 'Clap'
+
+metadata['Precond'] = np.nan
+metadata['Postcond'] = np.nan
+print(metadata)
+exit()
+try:
+    start_time = time.time()
+    print("RUNNING " + move1)
+    os.system("python " + directory + move1 + ".py " + ip + ' ' + str(port))
+    print("RUNNING " + move3)
+    os.system("python " + directory + move2 + ".py " + ip + ' ' + str(port))
+    supposed_duration = time.time()-start_time
+    actual_duration = moves[move1].duration+moves[move2].duration
+    if supposed_duration + 3 < actual_duration:
+        pass
+except Exception as e:
+    print("Error creating broker:", e)
